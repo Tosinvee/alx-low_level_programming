ba9996a9600bc834b5eb862c4ffeae2d185b5ce3
Tosinvee <bayodeoluwatosin06@gmail.com>
2022-10-13 20:10:00 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,13 @@
 #include <stdio.h>
 /**
- *
- * main is the beginning point
+ * main - is the beginning point
  * Return: Always 0 (Success)
  */
 
 int main(void)
-
 {
 
 	puts("\"Programming is like building a multilingual puzzle");
-	    return (0);
+	return (0);
 
 }
