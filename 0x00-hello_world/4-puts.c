1b4d1a9c8b6c810223239d130900847c6afbaf0d
Tosinvee <bayodeoluwatosin06@gmail.com>
2022-10-13 19:36:24 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ *
+ * main is the beginning point
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+
+{
+
+	puts("\"Programming is like building a multilingual puzzle");
+	    return (0);
+
+}
