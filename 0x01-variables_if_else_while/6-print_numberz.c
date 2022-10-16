d62db8a544d0fa62b0b859329066bbfc61fc9997
Tosinvee <bayodeoluwatosin06@gmail.com>
2022-10-15 17:06:05 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ *main - act as the starting point for the vawulence
+ *Return: zero for the vawulence
+ *
+ */
+
+int main(void)
+
+{
+
+	int a = 48;
+
+	for (; a < 58; a++)
+
+		putchar(a);
+
+	putchar('\n');
+
+	return (0);
+
+}
+
