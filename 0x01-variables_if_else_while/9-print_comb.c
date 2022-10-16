30d3931bdef2ab53b7c32abcc3d268386551f6f7
Tosinvee <bayodeoluwatosin06@gmail.com>
2022-10-15 17:50:18 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+ * main - act as the beginning point
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+
+{
+
+	int b;
+
+	for (b = 48; b < 58; b++)
+
+	{
+
+		putchar(r);
+
+		if (b != 57)
+
+		{
+
+			putchar(',');
+
+			putchar(' ');
+
+		}
+
+	}
+
+	putchar('\n');
+
+	return (0);
+
+}
