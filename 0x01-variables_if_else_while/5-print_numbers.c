38758663296c0f3323315d345f249bae22acf3ac
Tosinvee <bayodeoluwatosin06@gmail.com>
2022-10-15 17:01:39 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *Return:  0
+ */
+
+int main(void)
+
+{
+
+	int a = 0;
+
+	for (; a < 10 ; a++)
+
+		printf("%d", a);
+
+	putchar('\n');
+
+	return (0);
+
+}
