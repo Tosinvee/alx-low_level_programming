62bd489a3a38cd90aa211302e7a52ae463dfea50
Tosinvee <bayodeoluwatosin06@gmail.com>
2022-10-16 18:54:46 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,30 +1,17 @@
+#include <stdio.h>
 #include <unistd.h>
+
 /**
- *main-program entry point*
- *Return: 1 on success.
+ *main- acts as the begining point for the program *
+ *Return: value would be 1
  */
 
 int main(void)
 
 {
 
-		write(2, "and that piece of art is useful\" -Dora Korpar, 2015-10-19\n", 59);
-		return (1);
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+
+	return (1);
 
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
