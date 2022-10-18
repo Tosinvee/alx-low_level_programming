3a223b3c405e853f556dbf2349ad0f7934b79624
Tosinvee <bayodeoluwatosin06@gmail.com>
2022-10-18 15:28:02 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+
+/**
+ *print_alphabet- act as the beginning point
+ *Return: always return 0
+ */
+
+
+void print_alphabet(void)
+
+{
+
+		char c = 'a';
+
+
+
+		while (c <= 'z')
+
+		{
+
+			_putchar(c);
+
+			c++;
+
+
+
+		}
+
+		_putchar('\n');
+
+
+
+}
