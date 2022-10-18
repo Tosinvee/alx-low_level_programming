a4d1a267f742cd86ca0583096fe5053a80345eaf
Tosinvee <bayodeoluwatosin06@gmail.com>
2022-10-18 15:38:42 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+
+/**
+ *print_alphabet_x10 - acts as the entry point
+ *Return: zero always
+ */
+
+void print_alphabet_x10(void)
+
+{
+
+	char c;
+
+	int i = 0;
+
+
+
+	while (i <= 9)
+
+	{
+
+		for (c = 'a'; c <= 'z'; c++)
+
+		{
+
+			_putchar(c);
+
+		}
+
+
+
+		_putchar('\n');
+
+
+
+		i++;
+
+	}
+
+}
