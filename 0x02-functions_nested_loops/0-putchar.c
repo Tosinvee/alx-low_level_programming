a4253dfe51312ab721ba68e85ed9bf0c41d07c5a
Tosinvee <bayodeoluwatosin06@gmail.com>
2022-10-18 15:19:42 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,34 @@
 #include "main.h"
 
+
 /**
- * main - acts as the starting point of the
- * putchar
- * Return: 0
+ *main - act as the beginning point of the putchar
+ *Return: always zero
+ *
  */
+
 int main(void)
+
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	return (0);
+
+		_putchar('_');
+
+		_putchar('p');
+
+		_putchar('u');
+
+		_putchar('t');
+
+		_putchar('c');
+
+		_putchar('h');
+
+		_putchar('a');
+
+		_putchar('r');
+
+		_putchar('\n');
+
+		return (0);
+
 }
