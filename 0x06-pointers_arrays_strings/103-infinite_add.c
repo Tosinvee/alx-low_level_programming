ac6f6083ef905ed6d7cce6cb77cc332ce32531b9
Tosinvee <bayodeoluwatosin06@gmail.com>
2022-10-28 07:00:08 -0700

    0x06-pointers_arrays_strings

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -26,6 +26,16 @@ void rev_string(char *n)
 		*(n + i) = temp;
 	}
 }
+
+/**
+ * infinite_add - add 2  numbers together
+ * @n1: text representation of 1st number to add
+ * @n2: text representation of 2nd number to add
+ * @r: pointer to buffer
+ * @size_r: buffer size
+ * Return: pointer to calling function
+ */
+
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
 	int overflow = 0, i = 0, j = 0, digits = 0;
