858e918707fc63d293ed8d38ef94624ac66954ba
Tosinvee <bayodeoluwatosin06@gmail.com>
2022-10-30 05:40:11 -0700

    5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+
+/**
+ * rev_string - Reverses a string
+ * @s: The string to be modified
+ * Return: void
+ */
+
+void rev_string(char *s)
+
+{
+
+	char alp;
+
+	int i, len, leni;
+
+
+	len = 0;
+
+	leni = 0;
+
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+
+	leni = len - 1;
+
+	for (i = 0; i < len / 2; i++)
+	{
+		alp = s[i];
+		s[i] = s[leni];
+		s[leni--] = alp;
+	}
+}
