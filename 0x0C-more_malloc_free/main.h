48373560521cdc7eadab5cf7421cf7cd573a3aca
Tosinvee <bayodeoluwatosin06@gmail.com>
2022-11-27 12:00:14 -0800

    .

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -10,5 +10,7 @@ void _print(char *str, int l);
 char *mul(char n, char *num, int num_index, char *dest, int dest_index);
 int check_for_digits(char **av);
 void init(char *str, int l);
-
+int _putchar(char c);
+#include <stdio.h>
+#include <stdlib.h>
 #endif
